65e0e7e0d2eff92a59980d9d73e107066c1b36dd
Drew <glavich_drew@yahoo.com>
2021-05-15 18:58:16 +0900

    add address fields

--- src/pages/monshinhyou.js
+++ src/pages/monshinhyou.js
@@ -25,7 +25,9 @@ function FormikExample() {
           initialValues={{
             nameFurigana: "",
             name: "",
-            birthdate: { year: undefined, month: undefined, day: undefined }
+            birthdate: { year: undefined, month: undefined, day: undefined },
+            address: "",
+            addressFurigana: ""
           }}
           onSubmit={(values, actions) => {
             setTimeout(() => {
@@ -125,6 +127,41 @@ function FormikExample() {
                       </FormControl>
                     );
                   }}
+                </Field>
+                <Field name="address">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.address && form.touched.address}
+                      isRequired
+                    >
+                      <FormLabel htmlFor="address">住所</FormLabel>
+                      <Input {...field} id="address" placeholder="住所" />
+                      <FormErrorMessage>{form.errors.address}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="addressFurigana">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={
+                        form.errors.addressFurigana &&
+                        form.touched.addressFurigana
+                      }
+                      isRequired
+                    >
+                      <FormLabel htmlFor="addressFurigana">
+                        住所【カナ】
+                      </FormLabel>
+                      <Input
+                        {...field}
+                        id="addressFurigana"
+                        placeholder="住所【カナ】"
+                      />
+                      <FormErrorMessage>
+                        {form.errors.addressFurigana}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
                 </Field>
 
                 <Button
